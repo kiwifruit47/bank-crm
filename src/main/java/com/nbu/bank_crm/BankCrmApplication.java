21083b8ff0363f86a08c30dc81ec4f7dbde0398a
kiwifruit47 <victoria.dimitrovax@icloud.com>
2026-06-29 17:48:50 +0300

    init commit

---
+++ src/main/java/com/nbu/bank_crm/BankCrmApplication.java
@@ -0,0 +1,13 @@
+package com.nbu.bank_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankCrmApplication.class, args);
+	}
+
+}
