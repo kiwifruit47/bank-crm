9763a9aa50d1a8cc840419bd0d3dc11020a9969a
kiwifruit47 <victoria.dimitrovax@icloud.com>
2026-06-30 03:28:55 +0300

    added all-args constructor to CreateLegalEntityAccountRequest

--- src/main/java/com/nbu/bank_crm/dto/request/CreateLegalEntityAccountRequest.java
+++ src/main/java/com/nbu/bank_crm/dto/request/CreateLegalEntityAccountRequest.java
@@ -1,12 +1,14 @@
 package com.nbu.bank_crm.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Getter
+@AllArgsConstructor
 @RequiredArgsConstructor
 public class CreateLegalEntityAccountRequest {
     @NotBlank
